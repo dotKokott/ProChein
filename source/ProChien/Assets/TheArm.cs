ad17a6bf7f9adb676a41d7822c40b7d01cea5384
Christian Kokott <christian.kokott@gmail.com>
2015-10-01 22:35:33 +0200

    heyhey

--- source/ProChien/Assets/TheArm.cs
+++ source/ProChien/Assets/TheArm.cs
@@ -13,10 +13,14 @@ public class TheArm : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (!Pulling & Input.GetKeyDown(KeyCode.Space)) {
+        if (!Pulling && Input.GetKeyDown(KeyCode.Space)) {
             iTween.RotateTo(gameObject, iTween.Hash("z", (115f / 100f) * Mathf.Clamp(MicControlC.SoundVolumeValue / 2, 0, 100), "time", 1, "easetype", iTween.EaseType.easeOutBack, "oncomplete", "onComplete"));
             Pulling = true;
             iTween.ShakePosition(GameObject.Find("chi"), (new Vector3(0.2f, 0.2f) / 100f) * MicControlC.SoundVolumeValue / 2, 1);
+
+            if (MicControlC.SoundVolumeValue > 200) {
+                GameObject.Find("Fireworks").GetComponent<ParticleSystem>().Emit(10);
+            }
 
             Debug.Log(MicControlC.SoundVolumeValue);
         }
