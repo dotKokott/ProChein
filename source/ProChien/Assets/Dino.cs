c3e9f1e40b3f76e2b77f020381080df3ab3a66e4
Christian Kokott <christian.kokott@gmail.com>
2015-10-01 23:16:32 +0200

    uhuhu

---
+++ source/ProChien/Assets/Dino.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dino : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        iTween.RotateTo(gameObject, iTween.Hash("z", 18, "time", 2.5f, "easetype", iTween.EaseType.easeInBack, "looptype", iTween.LoopType.pingPong));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
