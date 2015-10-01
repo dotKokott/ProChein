9977a26b3d21c4cf324fe5232bfbd18d6c34f9ff
Christian Kokott <christian.kokott@gmail.com>
2015-10-01 22:22:17 +0200

    push it

--- source/ProChien/Assets/TheArm.cs
+++ source/ProChien/Assets/TheArm.cs
@@ -5,16 +5,18 @@ public class TheArm : MonoBehaviour {
 
     public bool Pulling = false;
 
+    public MicControlC control;
+
 	void Start () {
-	
+        control = Camera.main.GetComponent<MicControlC>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (!Pulling & Input.GetKeyDown(KeyCode.Space)) {
-            iTween.RotateTo(gameObject, iTween.Hash("z", 115, "time", 1, "easetype", iTween.EaseType.easeOutBack, "oncomplete", "onComplete"));
+            iTween.RotateTo(gameObject, iTween.Hash("z", (115f / 100f) * Mathf.Clamp(MicControlC.SoundVolumeValue, 0, 100), "time", 1, "easetype", iTween.EaseType.easeOutBack, "oncomplete", "onComplete"));
             Pulling = true;
-            iTween.ShakePosition(GameObject.Find("chi"), new Vector3(0.1f, 0.1f), 1);
+            iTween.ShakePosition(GameObject.Find("chi"), (new Vector3(0.2f, 0.2f) / 100f) * MicControlC.SoundVolumeValue, 1);
         }
 	}
 
@@ -27,6 +29,5 @@ public class TheArm : MonoBehaviour {
         yield return new WaitForSeconds(time);
 
         Pulling = false;
-
     }
 }
